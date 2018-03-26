c27f8a5bf248e8a80f368dc087001324de643f5a
Andrei <andreibalzan@gmail.com>
2018-03-26 10:31:43 +0200

    Currency-conversion-service is connecting to currency exchange service

---
+++ currency-conversion-service/src/main/java/com/andrei/microservices/model/CurrencyConvertionBean.java
@@ -0,0 +1,21 @@
+package com.andrei.microservices.model;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyConvertionBean {
+
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal conversionMultiple;
+	private BigDecimal quantity;
+	private BigDecimal totalCalculatedAmount;
+	private int port;
+}
