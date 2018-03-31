9ba7b78a7ae8a70b619d1dba58018b6ab3938499
Andrei <andreibalzan@gmail.com>
2018-03-31 11:16:51 +0200

    Ribbon(load balance) configuration

--- currency-conversion-service/src/main/java/com/andrei/microservices/service/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/andrei/microservices/service/CurrencyExchangeServiceProxy.java
@@ -1,5 +1,6 @@
 package com.andrei.microservices.service;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,9 +10,11 @@ import com.andrei.microservices.model.CurrencyConvertion;
 //you fill the name parameter with the application name present on application.properties
 //this is the name of the microservice you wanna call, in this case "currency-exchange-service"
 
-//you fill the url parameter, this I don't get yet, 
-//because if I have more than one microservice what is gonna happen?
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+// you fill the url parameter, this I don't get yet, 
+// because if I have more than one microservice what is gonna happen? for this we use Ribbon :)
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
 	// Here we need to define a method to talk with currency Exchange Service
