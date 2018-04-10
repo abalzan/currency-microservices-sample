35892eacb3e3e145d4e73cd7836b12c9380884be
Andrei <andreibalzan@gmail.com>
2018-04-10 21:42:59 +0200

    Configuring spring cloud bus

--- limits-service/src/main/java/com/andrei/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/andrei/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.andrei.microservices.limitsservice.controller;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.andrei.microservices.limitsservice.bean.LimitConfiguration;
 import com.andrei.microservices.limitsservice.config.Configuration;
 
+@RefreshScope
 @RestController
 @RequestMapping("/limits")
 public class LimitsConfigurationController {
