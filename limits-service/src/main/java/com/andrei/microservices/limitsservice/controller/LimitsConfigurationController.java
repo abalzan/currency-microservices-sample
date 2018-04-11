3f8d08034b67d57758d75875dcc5fc39e0d2218e
Andrei <andreibalzan@gmail.com>
2018-04-11 09:00:35 +0200

    configure Hystrix to fault tolerance.

--- limits-service/src/main/java/com/andrei/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/andrei/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -2,15 +2,14 @@ package com.andrei.microservices.limitsservice.controller;
 
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andrei.microservices.limitsservice.bean.LimitConfiguration;
 import com.andrei.microservices.limitsservice.config.Configuration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RefreshScope
 @RestController
-@RequestMapping("/limits")
 public class LimitsConfigurationController {
 
 	private Configuration configuration;
@@ -19,10 +18,24 @@ public class LimitsConfigurationController {
 		this.configuration = configuration;
 	}
 
-
-
-	@GetMapping
+	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfiguration() {
 		return new LimitConfiguration(configuration.getMinimum(), configuration.getMaximum());
+	}
+	
+	/**
+	 * This is an example, of how a microservice works, if some of the resource is unavailable,
+	 * if it is unavaliable, the fallbackRetrieveConfiguration method is called to assume some default value
+	 * to allow other microservices that depends on this limit service to work normally.
+	 * 
+	 */
+	@GetMapping("/fault-tolerance-sample")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Limit Service is not avaliable, Please check it!!!");
+	}
+	
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(9, 9999);
 	}
 }
